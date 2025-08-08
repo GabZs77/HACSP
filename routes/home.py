b4b75b6a069424a1954ea18f93701805d2828b72
GabZs77 <gabriel785779@gmail.com>
2025-08-08 14:27:41 -0300

    Update home.py

--- routes/home.py
+++ routes/home.py
@@ -1,15 +1,21 @@
+import os
 from flask import Blueprint, request, redirect, url_for, render_template
 
-bp_home = Blueprint('home', __name__, template_folder='../templates')
+# Caminho absoluto para a pasta de templates
+TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), '../templates')
 
-@bp_home.route('/home')
+bp_home = Blueprint('home', __name__, template_folder=TEMPLATE_DIR)
+
+@bp_home.route('/home', methods=['GET'])
 def home():
     auth_token = request.cookies.get('auth_token')
 
     if not auth_token:
-        return redirect(url_for('login.login'))  # Se não tem token, volta pro login
+        # Sem token, redireciona para o login
+        return redirect(url_for('login.login'))
 
-    # Aqui você poderia validar esse token com seu backend externo também,
-    # pra garantir que não é falso.
+    # Aqui você poderia validar esse token no backend externo
+    # if not validar_token(auth_token):
+    #     return redirect(url_for('login.login'))
 
     return render_template('home.html')
